dfc576cf8dd42daf68cb1b545963047db11080be
Robert <rcrooks@brightcove.com>
2021-12-12 14:50:43 -0800

    French studio translations updated

--- _site/assets/js/sajari-results-foot.js
+++ _site/assets/js/sajari-results-foot.js
@@ -11,7 +11,7 @@
   } else {
     lang = 'en-us';
   }
-  site_domain = lang_prefix + 'studio.support.brightcove.com';
+  site_domain = lang_prefix + 'fr.studio.support.brightcove.com';
   filter = "domain='" + site_domain +  "'";
   lang_filter = "lang='" + lang + "'";
   console.log('filter', filter);
